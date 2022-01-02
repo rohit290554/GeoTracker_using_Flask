43c972b44e0409c39980d71700f4d95411dc9815
Rohit Saxena <rohitsaxena722@gmail.com>
2022-01-02 23:15:21 +0530

    code_run_using Flask

---
+++ Core.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import urllib.request
+
+# IP address to test
+apiKey = 'cd443cee99fc491b8a49a7fd4db1a7c4'
+ipur = 'https://api.ipgeolocation.io/ipgeo?apiKey='
+ipurr = '&ip='
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+print('Target IP Addr is : '+external_ip)
+ip_address = external_ip
+# URL to send the request to
+request_url = ipur + apiKey + ipurr+ external_ip
+# Send request and decode the result
+response = requests.get(request_url)
+result = response.content.decode()
+result  = json.loads(result)
+
+IP=result['ip']
+continent_code = result['continent_code']
+continent_name = result['continent_name']
+country_code = result['country_code2']
+
+for key, value in result.items():
+    print(key, ' : ', value)
---
+++ t2.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from geopy.geocoders import Nominatim
+
+app = Flask(__name__)
+
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# Initialize Nominatim API
+def geotracker():
+    geolocator = Nominatim(user_agent="geoapiExercises")
+
+    # Assign Latitude & Longitude
+    Latitude = "26.4969"
+    Longitude = "80.3246"
+
+    # Displaying Latitude and Longitude
+    print("Latitude: ", Latitude)
+    print("Longitude: ", Longitude)
+
+    # Get location with geocode
+    location = geolocator.geocode(Latitude + "," + Longitude)
+    print(type(location))
+    # Display location
+    print("\nLocation of the given Latitude and Longitude:")
+    return str(location)
+
+
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
