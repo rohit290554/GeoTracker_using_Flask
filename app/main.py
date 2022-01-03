2c154f44babb3c621f9317ddf7dd9f1b49448c53
Rohit Saxena <rohitsaxena722@gmail.com>
2022-01-03 18:50:11 +0530

    heroku env setup

--- app/main.py
+++ app/main.py
@@ -14,11 +14,6 @@ app = Flask(__name__)
 def geotracker():
     ipur = 'https://ipapi.co/'
     ipurr = '/json/'
-    '''hostname = socket.gethostname()
-    ## getting the IP address using socket.gethostbyname() method
-    external_ip = socket.gethostbyname(hostname)
-    ## printing the hostname and ip_address
-    #external_ip = request.environ['REMOTE_ADDR']'''
     external_ip  = ip_address = flask.request.remote_addr
     '''urllib.request.urlopen('https://ident.me').read().decode('utf8')
         external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')'''
