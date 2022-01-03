12b7c30bb5de8f7ae34f6d4bf9add1efc05b8b5f
Rohit Saxena <rohitsaxena722@gmail.com>
2022-01-04 00:00:35 +0530

    fix 3.1.22 env setup test

--- app/main.py
+++ app/main.py
@@ -6,29 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
     return "<h1>Welcome to Geeks for Geeks</h1>"  # Initialize Nominatim API
-
-
-'''
-def geotracker():
-    ipur = 'https://ipapi.co/'
-    ipurr = '/json/'
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-    print('Target IP Addr is : ' + external_ip)
-    ip_address = external_ip
-    # URL to send the request to
-    request_url = ipur + external_ip + ipurr
-    # Send request and decode the result
-    response = requests.get(request_url)
-    result = response.content.decode()
-    result = json.loads(result)
-    test = json.dumps(result)
-    return test
-'''
-
-'''
-    try:
-        for key, value in result.items():
-            return key, ' : ', value
-    except:
-        return 'Error'
-'''
