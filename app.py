83067eb17afbd1e9e8f765b1ece1ab76f4f22d18
Rohit Saxena <rohitsaxena722@gmail.com>
2022-01-03 13:43:12 +0530

    v0.1

--- app.py
+++ app.py
@@ -30,10 +30,14 @@ def geotracker():
             return key, ' : ', value
     except:
         return 'Error'
+'''
+
+
+def main():
+    return geotracker
 
 
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
     app.run()
-'''
